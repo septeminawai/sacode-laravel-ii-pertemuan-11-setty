8d5a4f180952c0496d012490c2027a1c5453217c
sethy <89424441+septeminawai@users.noreply.github.com>
2024-09-13 12:32:40 +0900

    jalankan fungsi javascript dan mengerjakan quis

---
+++ script.js
@@ -0,0 +1,12 @@
+function perkalian(x, y) {
+    return x * y;
+}
+
+perkalian (4, 4);
+
+const btn = document.getElementById("btn");
+const tampilan = document.getElementById("tampilan");
+
+btn.onclick = function () {
+    tampilan.innerHTML = "Welcome Setty";
+}
